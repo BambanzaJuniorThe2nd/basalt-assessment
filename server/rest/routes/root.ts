6e810c5bd4191e1be0aa66584338fa23f34502ef
Vainqueur <bjthe2nd@gmail.com>
2024-01-16 18:32:19 +0200

    Implement default api endpoint

---
+++ server/rest/routes/root.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { ApiRequest, ApiResponse, StatusCode } from '..';
+
+export const root = Router();
+
+root.get('/', (res: ApiResponse) => {
+    res.status(StatusCode.SUCCESS).json({ ok : true });
+});
