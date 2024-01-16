8f0c58c9c72308ab1c1190b82f4feccaca7056a8
Vainqueur <bjthe2nd@gmail.com>
2024-01-16 17:53:10 +0200

    Define enum type for api error messages

---
+++ server/rest/messages.ts
@@ -0,0 +1,5 @@
+export enum ApiMessage {
+  ERROR_SERVER = "Server error occurred",
+  ERROR_ENDPOINT_DOES_NOT_EXIST = "Endpoint does not exist or incorrect HTTP method",
+  ERROR_RESOURCE_NOT_FOUND = "Resource not found",
+}
