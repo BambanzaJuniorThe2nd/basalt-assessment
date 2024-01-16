81c55993b56342be996ad1c4453e0685a99dd61b
Vainqueur <bjthe2nd@gmail.com>
2024-01-16 14:47:24 +0200

    Define interface ManagesDbs

--- server/core/types.ts
+++ server/core/types.ts
@@ -1,7 +1,15 @@
+import { Db, ObjectId } from 'mongodb';
+
 export interface CoreConfig {
   baseUrl: string;
   dbUrl: string;
   dbMain: string;
   dbPrefix: string;
   clientBaseUrl: string;
+}
+
+export interface ManagesDbs {
+  initialize(): Promise<void>;
+  mainDb(): Db;
+  db(name: string): Db;
 }
