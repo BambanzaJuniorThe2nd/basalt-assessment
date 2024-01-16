f7413806b99b5a5b36444191d54c3436cd94b4cf
Vainqueur <bjthe2nd@gmail.com>
2024-01-16 15:55:28 +0200

    Export util and bootstrap functions from core

--- server/core/index.ts
+++ server/core/index.ts
@@ -1,3 +1,6 @@
+import * as util from './util';
+export { util };
 export { CoreMessage } from './messages';
 export { ErrorCode, CoreError, throwDbError, throwValidationError } from './error';
 export * from './types';
+export { bootstrap } from './bootstrap';
