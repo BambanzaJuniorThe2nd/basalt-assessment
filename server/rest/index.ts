172f6e79109cc2ce4a78aa606c7d98f611098e3d
Vainqueur <bjthe2nd@gmail.com>
2024-01-16 19:57:54 +0200

    Mount ret API handler to server

--- server/rest/index.ts
+++ server/rest/index.ts
@@ -1,3 +1,8 @@
+import * as util from './util';
+export { util };
+export { getRouter } from './get-router';
+export { mountApi } from './mount-api';
 export { ApiMessage } from './messages';
 export { StatusCode } from './status-codes';
+export { injectCore, errorHandler, error404Handler } from './middleware';
 export * from './types';
---
+++ server/rest/mount-api.ts
@@ -0,0 +1,17 @@
+import { RestConfig, getRouter } from '.';
+import { Server } from '../server';
+import { Container } from '../core';
+import { error404Handler } from './middleware';
+import { ApiMessage } from '.';
+
+/**
+ * mounts rest API handler to server
+ * @param server server on which to attach API
+ * @param core core services container needed by the rest handler
+ * @param config config
+ */
+export const mountApi = (server: Server, core: Container, config: RestConfig) => {
+    const router = getRouter(core);
+    server.use(config.apiRoot, router);
+    server.use(() => error404Handler(ApiMessage.ERROR_RESOURCE_NOT_FOUND));
+};
